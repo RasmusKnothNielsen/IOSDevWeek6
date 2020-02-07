b9d8c483f8d2e5d6ba19cb289d702ffab9d40a81
Rasmus Knoth Nielsen <Rasmus.knoth@gmail.com>
2020-02-07 15:04:28 +0100

    Initial Commit

---
+++ Week6/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Week6
+//
+//  Created by Rasmus Knoth Nielsen on 07/02/2020.
+//  Copyright © 2020 Rasmus Knoth Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
